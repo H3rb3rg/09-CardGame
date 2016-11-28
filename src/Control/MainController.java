2bec08c718d760cd3478365523add71b544a408a
H3rb3rg <f-herberg@web.de>
2016-11-28 17:36:06 +0100

    sdf
    dsafsd

---
+++ src/Control/MainController.java
@@ -0,0 +1,37 @@
+package Control;
+
+import Model.Cards;
+import View.MainFrame;
+
+import javax.swing.*;
+
+
+/**
+ * Created by Jean-Pierre on 15.11.2016.
+ */
+public class MainController {
+
+    private MainFrame frame;
+    private Cards cards;
+    private JLabel newText;
+    private JLabel text;
+
+    public MainController(MainFrame frame){
+        this.frame = frame;
+        StackHandler newStack = new StackHandler(frame);
+        Cards newCard = new Cards("Heart","4",100,100);
+        frame.getActiveDrawingPanel().addObject(newCard);
+        addText();
+    }
+
+    public String getCardsValue(){
+        return this.cards.getValue();
+    }
+    public void addText() {
+        text = new JLabel("4");
+        newText.add(text);
+        newText.setBounds(100, 200, 200, 100);
+
+    }
+
+}
---
+++ src/Control/StackHandler.java
@@ -0,0 +1,13 @@
+package Control;
+
+import View.MainFrame;
+
+/**
+ * Created by 204g04 on 28.11.2016.
+ */
+public class StackHandler {
+
+    public StackHandler(MainFrame frame){
+
+    }
+}
